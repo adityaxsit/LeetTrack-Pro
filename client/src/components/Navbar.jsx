64bcaeb95a4a7d97b1eca9de783eec71e1fc379c
adityaxsit <1si24cs006@sit.ac.in>
2026-08-01 17:49:17 +0530

    Responsive navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import styles from './Navbar.module.css'; 
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+
+      <div className={styles.logo}>
+        <span className={styles.logoIcon}>#</span>
+        <span>LeetTrack Pro</span>
+      </div>
+
+      <div className={styles.search}>
+        <span>🔍</span>
+
+        <input
+          type="text"
+          placeholder="Search problems..."
+        />
+      </div>
+
+      <div className={styles.actions}>
+        <button className={styles.iconButton}>
+          🔔
+        </button>
+
+        <button className={styles.profileButton}>
+          👤
+        </button>
+      </div>
+
+    </nav>
+  );
+}
+export default Navbar;
